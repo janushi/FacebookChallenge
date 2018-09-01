be773c3edbdf8b8175583f69d96e3b90e8aa5e6e
janushi <37809843+janushi@users.noreply.github.com>
2018-09-01 15:40:03 +0530

    first commit

--- basic1.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "config.php";
-?>
-
-<!DOCTYPE html>
-<html lang="UTF-8">
-<input type="button" onclick="window.location = 'albumadin.php ';" value="Log In With Facebook" class="btn btn-primary"><br>
-
-</html>
